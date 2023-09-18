930af8b700b01b92a8503df9732db6375f9bd640
Darimo <pozzmo2022@gmail.com>
2023-09-19 00:54:10 +0700

    ngModel form with validation

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  Validation: string = "";
+
+  student_list: any = [
+    { name: "Miura Isuzu", age: 18, subject: "SpringBoot", is_save: 1 }
+  ];
+
+  subject: any = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "SpringBoot" },
+    { id: 3, name: "Angular" }
+  ];
+
+  add_Student: boolean = false;
+  add_btn: boolean = true;
+  onAddStudent() {
+    this.add_Student = true;
+    this.add_btn = false;
+
+    let item: any = {
+      name: "",
+      age: 0,
+      subject: 0,
+      is_save: 0
+    }
+    this.student_list.push(item);
+  }
+
+  save(stu: any) {
+    if (stu.name == "" || stu.age == 0 || stu.subject == 0) {
+      this.Validation = "Please Check your information";
+    }
+    else {
+      this.student_list.name = "";
+      this.student_list.age = "";
+      this.student_list.subject = "";
+      stu.is_save = 1;
+      this.add_Student = false;
+      this.add_btn = true;
+    }
+  }
+}
